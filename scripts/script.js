680735813c3880ff9eea2f0732f513c03b67ba07
helloalexdodd <hello@alexdodd.ca>
2019-06-15 15:00:55 -0400

    fixed a portfolio bug

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ portfolio.smoothScroll = () => {
 }
 
 portfolio.typed = new Typed('#typed', {
-	strings: ["^1000Javacsirpt Enthu", "Javascript Enthusiast.^2000", "^500 Front End Developer.^2000", "^500 Human.^1000", "^500Front End Developer."],
+	strings: ["^1300Javacsirpt Enthu", "Javascript Enthusiast.^2000", "^500 Front End Developer.^2000", "^500 Human.^1000", "^500Front End Developer."],
 	smartBackspace: true,
 	typeSpeed: 50,
 	startDelay: 1000,
@@ -20,6 +20,14 @@ portfolio.init = () => {
 
 $(function () {
 	portfolio.init();
-
+$(".toolBoxIcon").hover(
+  function() {
+    $(this).removeClass("wow slideInLeft");
+    $(this).addClass("animated pulse");
+  },
+  function() {
+    // $(this).removeClass("wow pulse");
+  }
+);
 
 });
